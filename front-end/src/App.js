8ac5a5c7a8a9d6a5fda30c35515a5563cd13b6a3
yuhanyuhan <yuhanlian@gmail.com>
2020-01-07 16:16:35 +0800

    Routed different functions accordingly to endpt as given

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import AddUser from "./AddUser";
+
+import { Layout, Menu } from "antd";
+const { Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{ background: "#fff" }}>
+        <Router>
+          <>
+            <Sider
+              style={{
+                overflow: "auto",
+                height: "100vh",
+                left: 0
+              }}
+            >
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={["1"]}
+                defaultOpenKeys={["sub1"]}
+                style={{ height: "100%" }}
+              >
+                <Menu.Item key="1">
+                  <Link to="/">Add New User</Link>
+                </Menu.Item>
+
+                <Menu.Item key="2">
+                  <Link to="/view_users">Edit Users</Link>
+                </Menu.Item>
+
+                <Menu.Item key="3">
+                  <Link to="/view_all">View Current Employee</Link>
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Layout>
+              <Content style={{ padding: "24px 0", minHeight: 280 }}>
+                <Switch>
+                  <Route exact path="/">
+                    <AddUser />
+                  </Route>
+                </Switch>
+              </Content>
+            </Layout> 
+          </>
+        </Router>
+      </Layout>
     </div>
   );
 }
